c6fc02f99e365e0198eee49d01c2f7be7f6721db
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-11-13 23:32:58 +0000

    most simple uploads

--- src/web/app.py
+++ src/web/app.py
@@ -1,15 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import send_from_directory
 from db import get_db, close_db
 import sqlalchemy
 from logger import log
 
+import os
+from flask import flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "/app/storage/"
+ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
+
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 app.teardown_appcontext(close_db)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS  # noqa: E501
+    )  # noqa: E501
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("download_file", name=filename))
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    """
 
 
 @app.route("/health")
@@ -21,7 +61,9 @@ def health():
         result = db.execute("SELECT NOW()")
         result = result.one()
         health = "OK"
-        log.info(f"/health reported OK including database connection: {result}")
+        log.info(
+            f"/health reported OK including database connection: {result}"
+        )  # noqa: E501
     except sqlalchemy.exc.OperationalError as e:
         msg = f"sqlalchemy.exc.OperationalError: {e}"
         log.error(msg)
@@ -30,3 +72,8 @@ def health():
         log.error(msg)
 
     return health
+
+
+@app.route("/uploads/<name>")
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
